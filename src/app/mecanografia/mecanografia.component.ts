d580c5c31b07d7a4858417f82bfc5b0b12e4e740
Huguini79 <166934655+Huguini79@users.noreply.github.com>
2026-08-20 23:05:04 +0200

    Add files via upload

--- src/app/mecanografia/mecanografia.component.ts
+++ src/app/mecanografia/mecanografia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ChangeDetectorRef} from '@angular/core';
 
 @Component
 ({
@@ -9,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 export class Mecanografia 
 {
+      constructor(private ref: ChangeDetectorRef) {}
     frase_original = "ffffaaaaffffssss";
     frase = this.frase_original;
     frase1 = "hhhhkkkkkkkk123";
     frase2 = "Hola Mundo!";
+    yeaah = "";
+    contador = 20;
+
     frase_array: any = 
     [
 
@@ -20,48 +24,101 @@ export class Mecanografia
     current_pos = 0;
     current_pos_phrase = 0;
     max_pos_phrase = 2;
+    longitud_frase_actual = 0;
+    correctas_de_frase_actual = 0;
+    incorrectas_de_frase_actual = 0;
 
     ngOnInit()
     {
         this.rellenarArray();
+        this.cuenta_a_atras();
     }
 
     rellenarArray()
     {
         this.frase_array = [];
+        this.longitud_frase_actual = 0;
+        this.correctas_de_frase_actual = 0;
+        this.incorrectas_de_frase_actual = 0;
         for (var i = 0; i < this.frase.length; ++i)
         {
+            this.longitud_frase_actual++;
             this.frase_array.push({char: this.frase[i], complete: false});
         }
     }
 
-    teclaPresionada(event: KeyboardEvent): void
+    cuenta_a_atras()
     {
-        if (event.key != "Shift")
-        {
-            if (event.key == this.frase[this.current_pos])
+        setInterval(() => {
+            if (this.contador > 0)
             {
-                this.frase_array[this.current_pos].complete = true;
-                this.current_pos++;
+                this.contador--;
+                this.ref.markForCheck();
 
             } else
             {
-                if (event.key == "Backspace")
+                if (this.correctas_de_frase_actual == this.longitud_frase_actual)
                 {
-                    if (this.current_pos != 0)
-                    {
-                        this.current_pos--;
-                    }
+                    this.yeaah = "Completado 🎉🥳🍾🥂!";
+                    alert("ENHORABUENA! HAS LOGRADO COMPLETAR LA FRASE ACTUAL");
+                    this.contador = 20;
+                    this.ref.markForCheck();
 
-                    this.frase_array[this.current_pos].complete = false;
-                
                 } else
                 {
-                    this.frase_array[this.current_pos].complete = null;
-                    this.current_pos++;
+                    alert("LO SIENTO! NO HAS LOGRADO SUPERAR LA FRASE ACTUAL");
+                    this.contador = 20;
+                    this.correctas_de_frase_actual = 0;
+                    this.incorrectas_de_frase_actual = 0;
+                    var textarea = document.getElementById('textarea') as HTMLInputElement;
+                    textarea.value = "";
+                    this.yeaah = "";
+                    this.ref.markForCheck();
                 }
             }
+           
+        }, 1000);
         
+    }
+
+    teclaPresionada(event: KeyboardEvent): void
+    {
+        if (this.correctas_de_frase_actual != this.longitud_frase_actual)
+        {
+            if (event.key != "Shift")
+            {
+                if (event.key == this.frase[this.current_pos])
+                {
+                    this.frase_array[this.current_pos].complete = true;
+                    this.current_pos++;
+                    this.correctas_de_frase_actual++;
+
+                } else
+                {
+                    if (event.key == "Backspace")
+                    {
+                        if (this.current_pos != 0)
+                        {
+                            this.current_pos--;
+                            if (this.frase_array[this.current_pos].complete == true)
+                            {
+                                this.correctas_de_frase_actual--;
+                            
+                            }
+                        }
+
+                        this.frase_array[this.current_pos].complete = false;
+
+                    } else
+                    {
+                        this.frase_array[this.current_pos].complete = null;
+                        this.current_pos++;
+                        this.incorrectas_de_frase_actual++;
+                    }
+                }
+            
+            }
+
         }
     }
 
